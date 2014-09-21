e0ce0d6ababc8468a75eb8e317de63d66a66d150
sorashiro <yufei930101@gmail.com>
2014-09-21 11:47:05 +0800

    add id to every item

--- router/routes/categories.js
+++ router/routes/categories.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var redis = require('redis');
+var client =  redis.createClient();
 
 router.get('/', function(req, res) {
   //TODO: Need to implement.
--- router/routes/items.js
+++ router/routes/items.js
@@ -3,30 +3,35 @@ var router = express.Router();
 var redis = require('redis');
 var client =  redis.createClient();
 
-var itemsList = [
-  {
-    'category': 'fruit', 'name': 'apple', 'unit': '斤', 'price': '5.50'
-  },
-  {
-    'category': 'fruit', 'name': 'leechee', 'unit': '斤', 'price': '15.00'
-  },
-  {
-    'category': 'food', 'name': 'sprite', 'unit': '瓶', 'price': '3.00'
-  },
-  {
-    'category': 'food', 'name': 'coca-cola', 'unit': '瓶', 'price': '3.00'
-  },
-  {
-    'category': 'livingGoods', 'name': 'battery', 'unit': '个', 'price': '2.00'
-  }
-];
-client.set('items', JSON.stringify(itemsList));
+function getItems() {
+  var items = [
+    {
+      'category': 'fruit', 'name': 'apple', 'unit': '斤', 'price': '5.50', id: '1'
+    },
+    {
+      'category': 'fruit', 'name': 'leechee', 'unit': '斤', 'price': '15.00', id: '2'
+    },
+    {
+      'category': 'food', 'name': 'sprite', 'unit': '瓶', 'price': '3.00', id: '3'
+    },
+    {
+      'category': 'food', 'name': 'coca-cola', 'unit': '瓶', 'price': '3.00', id: '4'
+    },
+    {
+      'category': 'livingGoods', 'name': 'battery', 'unit': '个', 'price': '2.00', id: '5'
+    }
+  ];
+  return items;
+}
+client.set('items', JSON.stringify(getItems()));
+
 
 router.get('/', function(req, res) {
   client.get('items', function(err, obj) {
-    res.send(JSON.parse(obj));
+    res.send(obj);
   });
 });
+
 
 
 module.exports = router;
