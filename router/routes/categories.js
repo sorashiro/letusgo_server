7e33ae57490212a0c2f12fbf30a7571915f64f89
sorashiro <yufei930101@gmail.com>
2014-09-22 14:01:48 +0800

    add get categories function

--- router/routes/categories.js
+++ router/routes/categories.js
@@ -3,6 +3,15 @@ var router = express.Router();
 var redis = require('redis');
 var client =  redis.createClient();
 
+function getCategories () {
+  var categories = [
+    {category: 'food', id: 1},
+    {category: 'fruit', id: 2},
+    {category: 'livingGoods', id: 3}
+  ];
+  return categories;
+}
+
 router.get('/', function(req, res) {
   //TODO: Need to implement.
   res.send('Success!');
