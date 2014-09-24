d623d3c60c7f7cbad96c56375a9d4440d072cafd
sorashiro <yufei930101@gmail.com>
2014-09-24 16:58:12 +0800

    update getItem function

--- router/routes/items.js
+++ router/routes/items.js
@@ -2,23 +2,24 @@ var express = require('express');
 var router = express.Router();
 var redis = require('redis');
 var client =  redis.createClient();
+var _ = require('lodash');
 
 function getItems() {
   var items = [
     {
-      'category': 'fruit', 'name': 'apple', 'unit': '斤', 'price': '5.50', id: '1'
+      category: 'fruit', name: 'apple', unit: '斤', price: '5.50', id: 1
     },
     {
-      'category': 'fruit', 'name': 'leechee', 'unit': '斤', 'price': '15.00', id: '2'
+      category: 'fruit', name: 'leechee', unit: '斤', price: '15.00', id: 2
     },
     {
-      'category': 'food', 'name': 'sprite', 'unit': '瓶', 'price': '3.00', id: '3'
+      category: 'food', name: 'sprite', unit: '瓶', price: '3.00', id: 3
     },
     {
-      'category': 'food', 'name': 'coca-cola', 'unit': '瓶', 'price': '3.00', id: '4'
+      category: 'food', name: 'coca-cola', unit: '瓶', price: '3.00', id: 4
     },
     {
-      'category': 'livingGoods', 'name': 'battery', 'unit': '个', 'price': '2.00', id: '5'
+      category: 'livingGoods', name: 'battery', unit: '个', price: '2.00', id: 5
     }
   ];
   return items;
