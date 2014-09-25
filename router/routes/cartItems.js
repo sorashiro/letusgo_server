c25a177f2b923f59bbb8e2122c5381960618b44d
sorashiro <yufei930101@gmail.com>
2014-09-25 14:23:06 +0800

    delete unuseful codes

--- router/routes/cartItems.js
+++ router/routes/cartItems.js
@@ -20,6 +20,7 @@ router.post('/', function(req, res) {
 router.post('/payment', function(req, res) {
   var cartItems = req.param('cartItems');
   var result = cartItems.length;
+
   if(result){
     client.del('cartItems');
     res.json(result);
@@ -29,66 +30,39 @@ router.post('/payment', function(req, res) {
   }
 });
 
-//router.post('/reduce', function(req, res) {
-//  client.get('cartItems', function(err, obj) {
-//    var cartItems = JSON.parse(obj);
-//    var id = req.param('id');
-//    var items = [];
-//
-//    _.forEach(cartItems, function(cartItem) {
-//      items.push(cartItem.item);
-//    });
-//    var index = _.findIndex(items, {'id': parseInt(id)});
-//    cartItems[index].num--;
-//
-//    if(cartItems[index].num === 0) {
-//      _.remove(cartItems, {num: 0});
-//    }
-//    client.set('cartItems', JSON.stringify(cartItems), function(err, obj) {
-//      res.send(obj);
-//    });
-//  });
-//});
+router.post('/reduce', function(req, res) {
 
-  router.post('/reduce', function(req, res) {
-    getCartItem(req.param('id'), function(data) {
-      var index = data[1];
-      var cartItems = data[0];
-      cartItems[index].num--;
-      if(cartItems[index].num === 0) {
-        _.remove(cartItems, {num: 0});
-      }
-      client.set('cartItems', JSON.stringify(cartItems), function (err, obj) {
-        res.send(obj);
-      });
-    });
-  });
-//router.post('/plus', function(req, res) {
-//  client.get('cartItems', function (err, obj) {
-//    var cartItems = JSON.parse(obj);
-//    var id = req.param('id');
-//    var items = _.pluck(cartItems, 'item');
-//    var index = _.findIndex(items, {'id': parseInt(id)});
-//    cartItems[index].num++;
-//
-//    client.set('cartItems', JSON.stringify(cartItems), function (err, obj) {
-//      res.send(obj);
-//    });
-//  });
-//});
-
-router.post('/plus', function(req,res) {
   getCartItem(req.param('id'), function(data) {
     var index = data[1];
     var cartItems = data[0];
-    cartItems[index].num++;
+    cartItems[index].num--;
+
+    if(cartItems[index].num === 0) {
+      _.remove(cartItems, {num: 0});
+    }
+
     client.set('cartItems', JSON.stringify(cartItems), function (err, obj) {
       res.send(obj);
     });
   });
 });
 
+router.post('/plus', function(req,res) {
+
+  getCartItem(req.param('id'), function(data) {
+    var index = data[1];
+    var cartItems = data[0];
+    cartItems[index].num++;
+
+    client.set('cartItems', JSON.stringify(cartItems), function (err, obj) {
+      res.send(obj);
+    });
+  });
+});
+
+
 function getCartItem(requireId, callback){
+
   client.get('cartItems', function(err, obj) {
     var cartItems = JSON.parse(obj);
     var id = requireId;
