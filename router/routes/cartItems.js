e2b38fdc21fbe56daf38539e033373f593bcd918
sorashiro <yufei930101@gmail.com>
2014-09-25 14:18:37 +0800

    update plus function

--- router/routes/cartItems.js
+++ router/routes/cartItems.js
@@ -50,18 +50,40 @@ router.post('/reduce', function(req, res) {
   });
 });
 
-router.post('/plus', function(req, res) {
-  client.get('cartItems', function (err, obj) {
-    var cartItems = JSON.parse(obj);
-    var id = req.param('id');
-    var items = _.pluck(cartItems, 'item');
-    var index = _.findIndex(items, {'id': parseInt(id)});
-    cartItems[index].num++;
+//router.post('/plus', function(req, res) {
+//  client.get('cartItems', function (err, obj) {
+//    var cartItems = JSON.parse(obj);
+//    var id = req.param('id');
+//    var items = _.pluck(cartItems, 'item');
+//    var index = _.findIndex(items, {'id': parseInt(id)});
+//    cartItems[index].num++;
+//
+//    client.set('cartItems', JSON.stringify(cartItems), function (err, obj) {
+//      res.send(obj);
+//    });
+//  });
+//});
 
+router.post('/plus', function(req,res) {
+  getCartItem(req.param('id'), function(data) {
+    var index = data[1];
+    var cartItems = data[0];
+    cartItems[index].num++;
     client.set('cartItems', JSON.stringify(cartItems), function (err, obj) {
       res.send(obj);
     });
   });
 });
+
+function getCartItem(requireId, callback){
+  client.get('cartItems', function(err, obj) {
+    var cartItems = JSON.parse(obj);
+    var id = requireId;
+    var items = _.pluck(cartItems, 'item');
+    var index = _.findIndex(items, {'id': parseInt(id)});
+    var data = [cartItems, index];
+    callback(data);
+  });
+}
 
 module.exports = router;
